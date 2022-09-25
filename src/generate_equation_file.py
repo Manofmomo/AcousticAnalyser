6102739871a56b4f9a053489533f8b63665f001d
Dhruv Bhatia <bhatiadhruv2001@gmail.com>
2022-09-25 22:48:27 +0530

    Added M0-M6, yet to verify

--- src/generate_equation_file.py
+++ src/generate_equation_file.py
@@ -1,3 +1,4 @@
+from locale import MON_1
 import sympy
 from sympy import I
 import pickle
@@ -9,9 +10,9 @@ def get_equation_free_end():
 
     x = sympy.symbols("x")
     w = sympy.symbols("w")
-    rho1, Area1, E1, I1 = sympy.symbols("rho1, Area1, E1, I1")
+    rho1, area1, E1, I1 = sympy.symbols("rho1, area1, E1, I1")
 
-    k1 = (rho1 * Area1 / (E1 * I1)) ** (0.25) * (w**0.5)
+    k1 = (rho1 * area1 / (E1 * I1)) ** (0.25) * (w**0.5)
 
     c1, d1 = sympy.symbols("c_traveling, d_traveling")
     c2, d2 = sympy.symbols("c_evanescent, d_evanescent")
@@ -45,9 +46,9 @@ def get_equation_fixed_end():
 
     x = sympy.symbols("x")
     w = sympy.symbols("w")
-    rho1, Area1, E1, I1 = sympy.symbols("rho1, Area1, E1, I1")
+    rho1, area1, E1, I1 = sympy.symbols("rho1, area1, E1, I1")
 
-    k1 = (rho1 * Area1 / (E1 * I1)) ** (0.25) * (w**0.5)
+    k1 = (rho1 * area1 / (E1 * I1)) ** (0.25) * (w**0.5)
 
     c1, d1 = sympy.symbols("c_traveling, d_traveling")
     c2, d2 = sympy.symbols("c_evanescent, d_evanescent")
@@ -79,57 +80,168 @@ def get_equation_fixed_end():
 
 # Assuming Wave equation to be A*e^(-I*k*x) + B*e^(-k*x) + C*e^(I*k*x) + D*e^(k*x)
 def get_equation_cross():
+    w = sympy.symbols("omega")
 
-    x = sympy.symbols("x")
-    w = sympy.symbols("w")
-    rho1, Area1, E1, I1 = sympy.symbols("rho1, Area1, E1, I1")
-    rho2, Area2, E2, I2 = sympy.symbols("rho2, Area2, E2, I2")
-
-    k1 = (rho1 * Area1 / (E1 * I1)) ** (0.25) * (w**0.5)
-    k2 = (rho2 * Area2 / (E2 * I2)) ** (0.25) * (w**0.5)
-
-    a1, b1, c1, d1 = sympy.symbols(
-        "a_traveling,  b_traveling, c_traveling, d_traveling"
+    rho1, area1, E1, I1, H1 = sympy.symbols("rho1, area1, E1, I1, H1")
+    K1 = (rho1 * area1 / (E1 * I1)) ** (0.25) * (w**0.5)
+    C1 = (E1 / rho1) ** (0.5)
+    alpha1 = (w * K1 / C1) ** (0.5)
+    beta1 = w * K1 / C1
+    temp = 0  # TODO
+    OMEGA = temp
+    zeta = temp
+    theta = temp
+    M1 = sympy.Matrix(
+        [
+            [
+                0,
+                0, 
+                zeta * OMEGA - I / OMEGA
+            ],
+            [
+                zeta * OMEGA - I, 
+                zeta * OMEGA + 1,
+                0
+            ],
+            [
+                1 + 0.5 * I * zeta * sympy.tan(theta / 2),
+                -1 - 0.5 * I * zeta * sympy.tan(theta / 2),
+                0,
+            ],
+        ]
     )
-    a2, b2, c2, d2 = sympy.symbols(
-        "a_evanescent, b_evanescent, c_evanescent, d_evanescent"
+
+    M2 = sympy.Matrix(
+        [
+            [
+                -I * sympy.sin(theta),
+                sympy.sin(theta), 
+                I * sympy.cos(theta) / OMEGA
+            ],
+            [
+                0.5
+                * I
+                * (
+                    (zeta**2) * (OMEGA**2) * sympy.tan(theta / 2)
+                    + 2 * sympy.cos(theta)
+                ),
+                0.5 * (zeta**2) * (OMEGA**2) * sympy.tan(theta / 2)
+                - sympy.cos(theta),
+                I * sympy.sin(theta) / OMEGA,
+            ],
+            [
+                (1 / 6)
+                * (
+                    I * (zeta**3) * (OMEGA**3)
+                    + 3 * I * zeta * OMEGA * sympy.tan(theta / 2)
+                    + 6
+                ),
+                (1 / 6)
+                * (
+                    (zeta**3) * (OMEGA**3)
+                    - 3 * zeta * OMEGA * sympy.tan(theta / 2)
+                    - 6
+                ),
+                0,
+            ],
+        ]
     )
-    params = [(a1, b1, c1, d1), (a2, b2, c2, d2)]
 
-    travelling_wave = sympy.exp(-I * k1 * x)
-    evanescent_wave = sympy.exp(-1 * k1 * x)
-    waves = [travelling_wave, evanescent_wave]
+    M3 = sympy.Matrix(
+        [
+            [
+                I * sympy.sin(theta),
+                -sympy.sin(theta),
+                I * sympy.cos(theta) / OMEGA
+            ],
+            [
+                -0.5
+                * I
+                * (
+                    (zeta**2) * (OMEGA**2) * sympy.tan(theta / 2)
+                    + 2 * sympy.cos(theta)
+                ),
+                -0.5 * (zeta**2) * (OMEGA**2) * sympy.tan(theta / 2)
+                + sympy.cos(theta),
+                -I * sympy.sin(theta) / OMEGA,
+            ],
+            [
+                -(1 / 6)
+                * I
+                * (
+                    (zeta**3) * (OMEGA**3)
+                    + 3 * zeta * OMEGA * sympy.tan(theta / 2)
+                    + 6 * I
+                ),
+                (1 / 6)
+                * (
+                    -(zeta**3) * (OMEGA**3)
+                    + 3 * zeta * OMEGA * sympy.tan(theta / 2)
+                    - 6
+                ),
+                0,
+            ],
+        ]
+    )
+    M4 = sympy.Matrix(
+        [
+            [
+                -sympy.sin(theta),
+                -sympy.sin(theta),
+                -sympy.cos(theta),
+            ],
+            [
+                -sympy.cos(theta), 
+                -sympy.cos(theta),
+                sympy.sin(theta),
+            ],
+            [
+                I,
+                1,
+                0,
+            ],
+        ]
+    )
+    M5 = sympy.Matrix(
+        [
+            [
+                -I * zeta * OMEGA * (sympy.sin(theta / 2) ** 2),
+                -zeta * OMEGA * (sympy.sin(theta / 2) ** 2),
+                -1,
+            ],
+            [
+                -1 - 0.5 * I * zeta * theta * sympy.sin(theta),
+                -1 - 0.5 * zeta * theta * sympy.sin(theta),
+                0,
+            ],
+            [
+                -I,
+                -1,
+                0,
+            ],
+        ]
+    )
+    M6 = sympy.Matrix(
+        [
+            [
+                I * zeta * OMEGA * (sympy.sin(theta / 2) ** 2),
+                zeta * OMEGA * (sympy.sin(theta / 2) ** 2),
+                -1,
+            ],
+            [
+                -1 + 0.5 * I * zeta * theta * sympy.sin(theta),
+                -1 + 0.5 * zeta * theta * sympy.sin(theta),
+                0,
+            ],
+            [
+                I,
+                1,
+                0,
+            ],
+        ]
+    )
 
-    final_equations = []
-
-    for i in range(2):
-        incoming_wave = waves[i]
-        a, b, c, d = params[i]
-
-        v1 = incoming_wave + c * sympy.exp(I * k1 * x) + d * sympy.exp(k1 * x)
-        v2 = a * sympy.exp(-I * k2 * x) + b * sympy.exp(-k2 * x)
-        v = sympy.Piecewise((v1, x < 0), (v2, x > 0))
-        vdiffs = [None] * 4
-        vdiffs[0] = v
-        for i in range(3):
-            vdiffs[i + 1] = sympy.diff(vdiffs[i], x)
-
-        eq1 = sympy.limit(vdiffs[0], x, 0, dir="-") - sympy.limit(
-            vdiffs[0], x, 0, dir="+"
-        )
-        eq2 = sympy.limit(vdiffs[1], x, 0, dir="-") - sympy.limit(
-            vdiffs[1], x, 0, dir="+"
-        )
-        eq3 = E1 * I1 * sympy.limit(vdiffs[2], x, 0, dir="-") - E2 * I2 * sympy.limit(
-            vdiffs[2], x, 0, dir="+"
-        )
-        eq4 = E1 * I1 * sympy.limit(vdiffs[3], x, 0, dir="-") - E2 * I2 * sympy.limit(
-            vdiffs[3], x, 0, dir="+"
-        )
-
-        final_equations.append([eq1, eq2, eq3, eq4])
-
-    return final_equations
+    return (M1, M2, M3, M4, M5, M6)
 
 
 if __name__ == "__main__":
